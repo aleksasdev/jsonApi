1d79842095acbca30619eed9c79e98f08ada1f3e
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-01-30 10:07:59 +0200

    implemented delete and post

--- src/jsonApi/Fetcher.js
+++ src/jsonApi/Fetcher.js
@@ -4,5 +4,33 @@ export default class Fetcher{
       this.id = id;
    }
 
-   
+   async post(body){
+      const response = await fetch(this.url, {
+         method: "POST",
+         headers:{
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(body)
+      })
+
+      this.handleResponse(response);
+   }
+
+   async delete(){
+      if(!this.id){
+         console.error("No id provided");
+         return;
+      }
+
+      const response = await fetch(`${this.url}/${this.id}`, {
+         method: "DELETE",
+      })
+      
+      this.handleResponse(response);
+   }
+
+   handleResponse(response){
+      if(!response?.ok) console.error(response.status);
+      else (console.log(response.status));
+   }
 }
